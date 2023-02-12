59e69c83b5aa14f555e9a4b4459fd74dc7fab542
Abinesh <rsabinesh.01@gmail.com>
2023-02-12 23:11:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let searchBox = document.querySelector(".search-box");
+
+function hidebtn () {
+
+    searchBox.classList.toggle("hide")
+}
+
+let menuBtn = document.querySelector("#menu-btn");
+let sideBtn = document.querySelector(".side-nav");
+  menuBtn.addEventListener('click',function(){
+    
+     sideBtn.classList.toggle("is-active")
+  })
+    
